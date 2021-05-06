f2b1e39326943cb6403f5940358fa767fc1c9f67
Deepak Singh <you@exampldeepakunicloud@gmail.com>
2021-05-06 18:36:02 +0530

    Change Tittle

--- Show-data.php
+++ Show-data.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <title>Hello, world!</title>
+    <title>Ajax!</title>
   </head>
   <body>
 
